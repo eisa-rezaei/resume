49f9c9f5baf5820e0f8337da7f59f9485c218692
jesus re <eisa.eisa.r.r@gmail.com>
2021-10-23 15:42:55 +0330

    performance2

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
-import flowApp from "../assets/projects/flow-app-landing.png";
+import flowApp from "../assets/projects/flow-app-landing.webp";
 import instagram from "../assets/projects/instagram.jpeg";
 import headphone_market from "../assets/projects/headphone.jpeg";
 import hotels_interview from "../assets/projects/hotels.jpg";
 import flowers_app from "../assets/projects/flowers.jpeg";
 import w3School_starter from "../assets/projects/w3school.jpg";
-import FoxxyAppLanding from "../assets/projects/foxxy-app-landing.jpg";
-import Spotify from "../assets/projects/spotify.png";
+import FoxxyAppLanding from "../assets/projects/foxxy-app-landing.webp";
+import Spotify from "../assets/projects/spotify.webp";
 
 import { SiStyledComponents } from "react-icons/si";
 
